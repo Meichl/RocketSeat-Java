1221affe5899095caddcebc86f9efa4f3a374f46
Michel <michel.geronimo@hotmail.com>
2024-02-25 18:54:26 -0300

    Iniciando o curso

---
+++ EstruturaDeDados.java
@@ -0,0 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class EstruturaDeDados {
+    public static void main(String[] args) {
+        //Lista
+        List<String> nomes = new ArrayList<>();
+        nomes.add("Mic");
+        nomes.add("Vic");
+
+
+        System.out.println(nomes.get(1));
+        for (String nome : nomes){
+            System.out.println("O nome é: " + nome);
+        }
+
+        nomes.forEach(nome -> System.out.println("O nome é: " + nome));
+        // ou
+        nomes.forEach(nome -> System.out.println("O nome que apareceu " + nome));
+    }
+
+
+
+}
---
+++ HelloWorld.java
@@ -0,0 +1,38 @@
+/*
+ * HelloWorld = Nome da minha classe
+ * public = tipo de acesso da classe
+ * class = tipo da classe
+ */
+
+public class HelloWorld {
+    //Todo Conteudo deverá ser inserido aqui dentro
+    public static void main(String[] args) {
+        /*
+         * Valores (int, double, float, long)
+         * Texto (String)
+         * Booleanos
+         */
+        int numero = 10;
+
+         //if - else
+         if (numero == 10){
+            System.out.println("Bacana");
+         } else if (numero == 12){
+            System.out.println("Não bacana");
+         } else{
+            System.out.println("Bacana 2");
+         }
+
+         //While (Enquanto algo for verdadeiro, faça alguma coisa)
+         int numero2 = 0;
+         while(numero2 < 3) {
+            System.out.println("Valor menor que 3");
+            numero2++;
+         }
+
+         //for
+         for (int i=0; i<4; i++){
+            System.out.println("Valor i é : " + i);
+         }
+    }
+}
